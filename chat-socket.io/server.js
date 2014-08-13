7a75b766ffd5390c725c35be88eb4bf45272048e
Renato M Camilio <renatomcamilio@gmail.com>
2014-08-13 17:58:51 -0300

    First steps for chat-socket.io - websockets chat spike solution

---
+++ chat-socket.io/server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/'));
+
+io.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+
+http.listen(8080);
+
