5a7f05f99922f0cbcdc3ffd3db70ecf9fa71b652
Renato M Camilio <renatomcamilio@gmail.com>
2014-08-14 20:45:47 -0300

    Changing files tree, moving apart client & server files

--- chat-socket.io/server/server.js
+++ chat-socket.io/server/server.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(__dirname + '/../client/'));
 
 io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
