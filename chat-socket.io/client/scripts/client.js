ff64bdfacb533292db8f79c6e76fec03be3abe69
Renato M Camilio <renatomcamilio@gmail.com>
2014-08-19 01:08:58 -0300

    Feature to chat in the global channel implemented.  - Users can send and receive message via WebSockets :)

--- chat-socket.io/client/scripts/client.js
+++ chat-socket.io/client/scripts/client.js
@@ -1,6 +1,71 @@
-var socket = io('/');
-socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-});
+var socket = io('/chat');
+
+// Message list
+var buildMessageHTML = function (message) {
+    return $('' +
+        '<div class="row">' +
+            '<div class="col-sm-12">' +
+                '<p class="btn btn-' + (message.author.id == socket.io.engine.id ? 'success pull-right' : 'default') + ' user-msg">' +
+                    '<strong>' + (message.author.name || message.author.id) + ': </strong>' +
+                    message.text +
+                '</p>' +
+            '</div>' +
+        '</div>');
+};
+
+var updateMessageList = function (message) {
+    console.log(arguments);
+    $('.chat .message-list .well').append( buildMessageHTML(message) );
+};
+
+// Message sender
+var configMessageSender = function (person) {
+    var $messageSender = $('.message-sender');
+
+    $messageSender.attr('data-author', person.id);
+    $messageSender.on('click', '.send-message', function () {
+        var $messageTxt = $messageSender.find('[name="message-text"]');
+        var messageTxt = $messageTxt.val().trim();
+
+        if (! messageTxt.length) return false;
+
+        var message = {
+            text: messageTxt,
+            author: person
+        };
+
+        socket.emit('send:message', message);
+
+        $messageTxt.val('');
+    });
+};
+
+// People list
+var buildPersonHTML = function (person) {
+    var $person = $('<a href="#" class="list-group-item" />');
+    $person.attr('data-person-id', person.id);
+    $person.html( person.name || person.id );
+
+    if (person.id == socket.io.engine.id) {
+        configMessageSender(person);
+        $person.append('<span class="label label-warning pull-right">me</span>');
+    }
+
+    return $person;
+};
+
+var updatePeopleList = function (people) {
+    var peopleList = [];
+
+    _.each(people, function (person) {
+        peopleList.push( buildPersonHTML(person) );
+    });
+
+    $('.chat .people-list').html(peopleList);
+};
+
+// websockets events
+socket.on('update:people-list', updatePeopleList);
+socket.on('update:message-list', updateMessageList);
+console.log(socket);
 
--- chat-socket.io/server/server.js
+++ chat-socket.io/server/server.js
@@ -1,16 +1,35 @@
+var _ = require('lodash');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var chat = io.of('/chat');
+
 app.use(express.static(__dirname + '/../client/'));
 
-io.on('connection', function (socket) {
-    socket.emit('news', { hello: 'world' });
-    socket.on('my other event', function (data) {
-        console.log(data);
+http.listen(8080);
+
+// sockets events
+var getPeopleList = function () {
+    var peopleList = [];
+
+    _.each(chat.sockets, function (socket) {
+        peopleList.push( _.pick(socket, ['id']) );
+    });
+
+    return peopleList;
+};
+
+chat.on('connection', function (socket) {
+    chat.emit('update:people-list', getPeopleList());
+
+    socket.on('disconnect', function () {
+        chat.emit('update:people-list', getPeopleList());
+    });
+
+    socket.on('send:message', function (message) {
+        chat.emit('update:message-list', message);
     });
 });
-
-http.listen(8080);
 
