95a34d2fd6f0360c6f0e0c93808f1d89e3002c36
Renato M Camilio <renatomcamilio@gmail.com>
2014-08-20 23:53:36 -0300

    Organising module initialization and config

---
+++ chat-socket.io/server/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var http = http.Server(app);
+var chat = require('./chat');
+
+// Routes and assets
+app.use(express.static(__dirname + '/../client/'));
+
+// start http server listening on 8080
+http.listen(8080);
+
